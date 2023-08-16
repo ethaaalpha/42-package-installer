2848ddda4026385bfc875f8e6f6c7271524285c5
Ethaniel Billon <ebillon@z2r1p6.42lyon.fr>
2023-08-16 16:56:44 +0200

    major fix

--- script-installer.sh
+++ script-installer.sh
@@ -7,8 +7,20 @@ rm -rf $VSCODE_FINAL
 
 echo "Installing zsh"
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+echo "Fixing 42 school VSCode too"
+echo "{
+  \"terminal.integrated.defaultProfile.linux\": \"bash\",
+  \"terminal.integrated.profiles.linux\": {
+    \"bash\": {
+      \"path\": \"/usr/bin/flatpak-spawn\",
+      \"args\": [\"--host\", \"--env=TERM=xterm-256color\", \"zsh\"]
+    }
+  }
+}" > ~/.var/app/com.visualstudio.code/config/Code/User/settings.json
+
 echo "Downloading vscode"
-wget 'https://code.visualstudio.com/sha/download?build=stable&os=linux-x64' -O $VSCODE_DOWN
+cp -r "/nfs/sgoinfre/goinfre/Perso/ebillon/$VSCODE_DOWN" .
 echo "Extracting vscode"
 sleep 1
 tar -xf $VSCODE_DOWN
@@ -23,14 +35,3 @@ cp code.desktop ~/Desktop/
 PATHTOADD="$PWD/$VSCODE_FINAL"
 echo "export PATH=$PATH:$PATHTOADD" >> ~/.zshrc
 echo 'alias code="nohup code . > /dev/null & disown"' >> ~/.zshrc
-
-echo "Fixing 42 school VSCode too"
-echo "{
-  \"terminal.integrated.defaultProfile.linux\": \"bash\",
-  \"terminal.integrated.profiles.linux\": {
-    \"bash\": {
-      \"path\": \"/usr/bin/flatpak-spawn\",
-      \"args\": [\"--host\", \"--env=TERM=xterm-256color\", \"zsh\"]
-    }
-  }
-}" >> ~/.var/app/com.visualstudio.code/config/Code/User/settings.json
